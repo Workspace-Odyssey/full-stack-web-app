b504bd9f033c1c11e9be0cd3dc68d41f36693d51
Joe Reeve <joe.reeve@gmail.com>
2025-02-12 14:42:19 +0900

    test push

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,15 @@
+require('dotenv').config({ path: './.env.local' });
 const express = require('express');
 const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
-const cors = require("cors");
+const cors = require('cors');
 const knex = require('./knex');
 const coworkingSpacesRoutes = require('./routes/coworkingSpaceRoutes');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
 const reviewsRoutes = require('./routes/reviewRoutes');
-const path = require("path");
+const path = require('path');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
@@ -18,48 +19,54 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 // CORS configuration for development
 if (!isProduction) {
-	app.use(cors({
-		origin: 'http://localhost:5173',
-		methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-		credentials: true, // Allow cookies to be sent with requests
-	}));
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true, // Allow cookies to be sent with requests
+    })
+  );
 }
 
-app.use(session({
-	secret: process.env.EXPSECRET,
-	resave: false,
-	saveUninitialized: false,
-	cookie: {
-		secure: process.env.NODE_ENV === 'production',
-		httpOnly: true,
-		maxAge: 1000 * 60 * 60 * 24, // Set the cookie expiration to 1 day
-		sameSite: 'lax',
-	},
-	store: new MemoryStore({
-		checkPeriod: 86400000 // prune expired entries every 24h
-	}),
-}));
+app.use(
+  session({
+    secret: process.env.EXPSECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, // Set the cookie expiration to 1 day
+      sameSite: 'lax',
+    },
+    store: new MemoryStore({
+      checkPeriod: 86400000, // prune expired entries every 24h
+    }),
+  })
+);
 
 // Serve static files in production
 if (isProduction) {
-	app.use(cors({
+  app.use(
+    cors({
       origin: 'https://workspace-odyssey.onrender.com', // Allowing requests from the deployed frontend
       methods: ['GET', 'POST'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
-	}));
+    })
+  );
 
-	// Serve static files from the 'dist' directory
-	app.use(express.static(path.join(__dirname, '../../client/dist')));
+  // Serve static files from the 'dist' directory
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-	app.use('/user', authRoutes);
-	app.use('/reviews', reviewsRoutes);
-	app.use('/coworking_spaces', coworkingSpacesRoutes);
+  app.use('/user', authRoutes);
+  app.use('/reviews', reviewsRoutes);
+  app.use('/coworking_spaces', coworkingSpacesRoutes);
 
-  	app.get('*', (req, res) => {
-	  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
-	});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+  });
 }
 
 // API routes
@@ -67,7 +74,6 @@ app.use('/user', authRoutes);
 app.use('/reviews', reviewsRoutes);
 app.use('/coworking_spaces', coworkingSpacesRoutes);
 
-
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}!`);
 });
--- server/src/util/geocoding.js
+++ server/src/util/geocoding.js
@@ -3,25 +3,23 @@ const apiKey = process.env.GOOGLE_MAPS_API_KEY;
 const axios = require('axios');
 
 async function getGeocodingData(address) {
-  
-    try {
-      const response = await axios.get(GEOCODING_API_URL, {
-        params: { address, key: apiKey },
-      });
-  
-      const results = response.data.results
-  
-      if (response.data.status === 'OK' && results && results[0]) {
-          const { lat, lng } = results[0].geometry.location;
-          return { lat, 'long': lng };
-      } else {
-          console.error(`Error fetching coordinates: ${response.data.status}`);
-      }   
-    } catch (error) {
-      console.error('Error in geocoding service:', error);
-      throw error;
+  try {
+    const response = await axios.get(GEOCODING_API_URL, {
+      params: { address, key: apiKey },
+    });
+
+    const results = response.data.results;
+
+    if (response.data.status === 'OK' && results && results[0]) {
+      const { lat, lng } = results[0].geometry.location;
+      return { lat, long: lng };
+    } else {
+      console.error(`Error fetching coordinates: ${response.data.status}`);
     }
+  } catch (error) {
+    console.error('Error in geocoding service:', error);
+    throw error;
+  }
 }
 
 module.exports = getGeocodingData;
-  
