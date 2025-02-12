329de9e345b66e62d83777290a6b2df29b77450d
Joe Reeve <joe.reeve@gmail.com>
2025-02-12 18:33:47 +0900

    feat: more detailed review metrics are now added to the database

--- server/db/migrations/20250205030905_create_reviews_table.js
+++ server/db/migrations/20250205030905_create_reviews_table.js
@@ -3,20 +3,31 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-    await knex.schema.createTable('reviews', (table) => {
-        table.increments('id').primary();
-        table.text('content').nullable();
-        table.integer('stars').notNullable().checkBetween([1, 5]);
-        table.uuid('user_id').references('uuid').inTable('users').onDelete('CASCADE');
-        table.uuid('coworking_id').references('uuid').inTable('coworkings').onDelete('CASCADE');
-        table.date('created_at').notNullable();
-    });
-}
+  await knex.schema.createTable('reviews', (table) => {
+    table.increments('id').primary();
+    table.text('content').nullable();
+    table.integer('stars').notNullable().checkBetween([1, 5]);
+    table.integer('net_rating').checkBetween([1, 5]);
+    table.integer('comfort_rating').checkBetween([1, 5]);
+    table.integer('noise_rating').checkBetween([1, 5]);
+    table.integer('cost_rating').checkBetween([1, 5]);
+    table
+      .uuid('user_id')
+      .references('uuid')
+      .inTable('users')
+      .onDelete('CASCADE');
+    table
+      .uuid('coworking_id')
+      .references('uuid')
+      .inTable('coworkings')
+      .onDelete('CASCADE');
+    table.date('created_at').notNullable();
+  });
+};
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('reviews');
+  return knex.schema.dropTableIfExists('reviews');
 };
-
--- server/src/models/reviewsModel.js
+++ server/src/models/reviewsModel.js
@@ -8,60 +8,63 @@ const validateProps = validProps([
   'stars',
   'user_id',
   'coworking_id',
-  'created_at'
+  'created_at',
+  'net_rating',
+  'comfort_rating',
+  'noise_rating',
+  'cost_rating',
 ]);
 
 const validateRequired = requiredProps([
-    'stars',
-    'user_id',
-    'coworking_id',
-    'created_at'
+  'stars',
+  'user_id',
+  'coworking_id',
+  'created_at',
 ]);
 
 const REVIEW_TABLE = 'reviews';
 
-
 module.exports = {
-    REVIEW_TABLE,
+  REVIEW_TABLE,
 
-    submit(review) {
-        validateRequired(validateProps(review));
-        return knex(REVIEW_TABLE).insert(review);
-    },
+  submit(review) {
+    validateRequired(validateProps(review));
+    return knex(REVIEW_TABLE).insert(review);
+  },
 
-    getNumberOfRatingsByCoWorkingId(id) {
-        return knex(REVIEW_TABLE)
-            .count('* as number_of_ratings')
-            .where({ 'coworking_id': id })
-            .first()
-            .then(result => result.number_of_ratings)
-    },
+  getNumberOfRatingsByCoWorkingId(id) {
+    return knex(REVIEW_TABLE)
+      .count('* as number_of_ratings')
+      .where({ coworking_id: id })
+      .first()
+      .then((result) => result.number_of_ratings);
+  },
 
-    getAverageRatingByCoWorkingId(id) {
-        return knex(REVIEW_TABLE)
-            .avg('stars as average_rating')
-            .where({ 'coworking_id': id })
-            .first()
-            .then(result => result.average_rating)
-    },
+  getAverageRatingByCoWorkingId(id) {
+    return knex(REVIEW_TABLE)
+      .avg('stars as average_rating')
+      .where({ coworking_id: id })
+      .first()
+      .then((result) => result.average_rating);
+  },
 
-    filterByCoWorkingId(id) {
-        return knex
-            .select({
-                id: `${REVIEW_TABLE}.id`,
-                rating: `${REVIEW_TABLE}.stars`,
-                datePosted: `${REVIEW_TABLE}.created_at`,
-                reviewText: `${REVIEW_TABLE}.content`,
-                username: 'u.username',
-            })
-            .from(REVIEW_TABLE)
-            .innerJoin('coworkings as c', function () {
-                this.on('c.uuid', '=', `${REVIEW_TABLE}.coworking_id`);
-            })
-            .innerJoin('users as u', function () {
-                this.on('u.uuid', '=', `${REVIEW_TABLE}.user_id`);
-            })
-            .where({ [`${REVIEW_TABLE}.coworking_id`]: id })
-            .orderBy(`${REVIEW_TABLE}.created_at`, 'desc')
-    }
-}
+  filterByCoWorkingId(id) {
+    return knex
+      .select({
+        id: `${REVIEW_TABLE}.id`,
+        rating: `${REVIEW_TABLE}.stars`,
+        datePosted: `${REVIEW_TABLE}.created_at`,
+        reviewText: `${REVIEW_TABLE}.content`,
+        username: 'u.username',
+      })
+      .from(REVIEW_TABLE)
+      .innerJoin('coworkings as c', function () {
+        this.on('c.uuid', '=', `${REVIEW_TABLE}.coworking_id`);
+      })
+      .innerJoin('users as u', function () {
+        this.on('u.uuid', '=', `${REVIEW_TABLE}.user_id`);
+      })
+      .where({ [`${REVIEW_TABLE}.coworking_id`]: id })
+      .orderBy(`${REVIEW_TABLE}.created_at`, 'desc');
+  },
+};
